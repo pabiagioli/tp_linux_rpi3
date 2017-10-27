536cdab3f10bcede4602d1383aedee08f5bc4a78
Pablo Biagioli <pabiagioli@gmail.com>
2017-10-27 03:03:18 -0300

    led operations finished. Added jansson lib already compiled

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH::jansson-lib/lib/pkgconfig/
 echo "--------BEGIN COMPILE SOURCES--------"
-aarch64-rpi3-gcc -v -g led_operator.c  -I/home/pampa/dev-folder/linux-embedded/facu/tpfinal/jansson/arm64-lib/include -I/home/pampa/dev-folder/linux-embedded/facu/tpfinal/jansson/arm64-lib/lib `pkg-config --cflags --libs jansson`
+aarch64-rpi3-gcc -v -g led_operator.c  -I./jansson-lib/include -I./jansson-lib/lib `pkg-config --cflags --libs jansson`
 echo "--------END COMPILE SOURCES--------"
 
 echo "--------BEGIN COPY BINARY TO RASPBERRY PI 3--------"
-cp -v a.out ~/linux-embedded-labs/nfs-root/led_ops
+cp -v a.out ~/linux-embedded-labs/nfs-root/www/cgi-bin/led_ops
+cp -v environment ~/linux-embedded-labs/nfs-root/www/cgi-bin/environment
+cp -v index.html ~/linux-embedded-labs/nfs-root/www
 echo "--------END COPY BINARY TO RASPBERRY PI 3--------"
--- led_operator.c
+++ led_operator.c
@@ -1,6 +1,7 @@
 #include <limits.h> /* PATH_MAX */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
@@ -19,6 +20,7 @@ typedef struct led_obj_list {
     led_object_t* leds;
     int listSize;
 } led_list_t;
+
 /**
  * GET TRIGGERS FROM TEXT FILE ON DEVICE FOLDER
  * --------------------------------------------
@@ -57,11 +59,15 @@ char* getTriggers(char *devicePath){
         result[ptr] = '\0';
         fclose(fd);
     }
-    //printf("%s \n",result);
 
     return result;
 }
 
+/**
+ * GET LEDS
+ * --------
+ * 
+ * */
 led_list_t getLeds (const char *ledFolder){
     led_list_t result;
     led_object_t *ledObjs;// = malloc(sizeof(led_object_t));
@@ -117,9 +123,30 @@ led_list_t getLeds (const char *ledFolder){
     return result;
 }
 
+int min(int a, int b){
+    if (a < b)
+        return a;
+    if (b < a)
+        return b;
+}
+
+
+void changeTrigger(char *led, char *trigger) {
+    
+    char triggerFilePath [PATH_MAX]; 
+    struct stat triggerFilePathStat;
+    sprintf(triggerFilePath, "/sys/devices/platform/leds/leds/%s/%s",led,"trigger");
+    //sprintf(triggerFilePath, "echo %s > /sys/devices/platform/leds/leds/%s/%s",trigger, led,"trigger");
+    FILE *fd = fopen(triggerFilePath, "w");
+    fputs(trigger,fd);
+    fflush(fd);
+    fclose(fd);
+    //execute a shell command (For extreme USERS)
+    //system(triggerFilePath);
+}
+
 int main(int argc, char **argv) {
 
-    json_t *root;
     json_t *led_array;
     json_t *triggers;
 
@@ -127,30 +154,61 @@ int main(int argc, char **argv) {
     led_list_t list;
     int i = 0;
 
-    if(argc > 1)
-        dirArg = argv[1];
-    else
-        dirArg = "/sys/class/leds";
+    char *httpMethod = getenv("REQUEST_METHOD");
+    char *queryString;
+    
+    if(httpMethod && strcmp(httpMethod,"GET") == 0){
+        queryString = getenv("QUERY_STRING");
+        //parse query string for the dirArg;
+        if(queryString && strlen(queryString) > 0)
+            dirArg = queryString;
+        else    
+            dirArg = "/sys/class/leds";
+    }else if (httpMethod && strcmp(httpMethod,"POST") == 0){
+        char Buffer[512]={0};
+        int InputLength = atoi( getenv("CONTENT_LENGTH") );
+        InputLength = min( InputLength, sizeof(Buffer)-1 ); /* Avoid buffer overflow */
+        fread( Buffer, InputLength, 1, stdin );
+        json_t *request = json_loads(Buffer,JSON_DECODE_ANY, NULL);
+        /* POST variables */
+        char *ledStr, *triggerStr;
+        //json_unpack(request, "{s:s,s:s}", "led", ledStr, "trigger", triggerStr);
+        ledStr = (char *) json_string_value(json_object_get(request,"led"));
+        triggerStr = (char *) json_string_value(json_object_get(request,"trigger"));
+        
+        changeTrigger(ledStr, triggerStr);
+
+        printf("Content-type: text/html\n\n");
+        printf("%s %s \n",ledStr, triggerStr);
+        free(request);
+        return 0;
+
+    }else{
+        if(argc > 1)
+            dirArg = argv[1];
+        else
+            dirArg = "/sys/class/leds";
+    }
 
     list = getLeds(dirArg);
 
-    //root = json_object();
     led_array = json_array();
-
     
     for(i = 0; i< list.listSize; i++){
         //printf("led = %s , dir = %s, triggers = %s \n", list.leds[i].led, list.leds[i].ledDir, list.leds[i].ledTriggers);
         json_auto_t *ledObj = json_object();
-        json_object_set(ledObj,"led",json_string(list.leds[i].led));
-        json_object_set(ledObj,"triggers",json_string(list.leds[i].ledTriggers));
+        json_object_set(ledObj, "led", json_string(list.leds[i].led));
+        json_object_set(ledObj, "triggers", json_string(list.leds[i].ledTriggers));
         json_array_append(led_array, ledObj);
-        json_decref(ledObj);
+        //json_decref(ledObj);
     }
     //json_decref(led_array);
     //char *output = json_dumps(led_array, JSON_INDENT(4));
     char *output = json_dumps(led_array, JSON_COMPACT);
+    printf("Content-type:application/json\n\n");
     printf("%s \n", output);
     free(output);
+
     return 0;
 
 }
